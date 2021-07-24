ec0be6d47a4efaa28570155ad0995aacbaefb220
seo jin <comcxx11@gmail.com>
2021-07-24 13:51:45 +0900

    First Commit

---
+++ Example/Pods/Target Support Files/Pods-SPMenu_Tests/Pods-SPMenu_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPMenu_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SPMenu_Tests
+@end
---
+++ Example/Pods/Target Support Files/SPMenu/SPMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SPMenu : NSObject
+@end
+@implementation PodsDummy_SPMenu
+@end
