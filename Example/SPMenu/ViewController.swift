39973f531a6941b18bed05b70c28f02fb37e9989
seo jin <comcxx11@gmail.com>
2021-07-24 15:24:27 +0900

    Create SPMenuData

--- Example/SPMenu/ViewController.swift
+++ Example/SPMenu/ViewController.swift
@@ -9,8 +9,29 @@
 import UIKit
 import SPMenu
 
+struct cashList: Codable {
+    var sortNo: String?
+    var cashName: String?
+    var amounts: String?
+    var maxVal: String?
+    var minVal: String?
+    var hint: String?
+}
+
+struct inboundChannels: Codable {
+    var sortNo: Int?
+    var downloadUrl: String?
+    var text: String?
+    var type: Int?
+    var url: String?
+}
+
+
+
 class ViewController: UIViewController {
 
+    var data = ["a", "b"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +40,21 @@ class ViewController: UIViewController {
         let origin = sender.frame.origin
         let x = origin.x
         let y = origin.y + sender.frame.size.height
-        let a = SPMenu(frame: self.view.frame, offSet: CGPoint(x: x, y: y))
+        
+        let d1 = [inboundChannels(sortNo: 1, downloadUrl: "10,000", text: "bb", type: 1, url: ""),
+                  inboundChannels(sortNo: 2, downloadUrl: "20,000", text: "yy", type: 1, url: "")]
+        
+        var d: [SPMenuData<inboundChannels>] = []
+        for i in d1 {
+            d.append(SPMenuData(title: i.text, data: i))
+        }
+        
+        let a = SPMenu<inboundChannels>(frame: self.view.frame, offSet: CGPoint(x: x, y: y))
+        a.reloadData(data: d)
+        a.selectItem = { item in
+            print(item?.downloadUrl ?? "")
+        }
+        
         self.view.addSubview(a)
     }
     
--- SPMenu/Classes/SPMenu.swift
+++ SPMenu/Classes/SPMenu.swift
@@ -1,11 +1,26 @@
 import UIKit
 
-open class SPMenu: UIView {
-    var dim = UIView(frame: CGRect.zero)
-    var menu: UITableView?
-    var offset: CGPoint?
+
+public struct SPMenuData<T> {
+    public init(title: String?, data: T) {
+        self.title = title
+        self.data = data
+    }
+    public var title: String?
+    public var data: T
+}
+
+
+open class SPMenu<T>: UIView, UITableViewDataSource, UITableViewDelegate {
     
-    public convenience init(frame: CGRect, offSet: CGPoint) {
+    var dim = UIView(frame: CGRect.zero)
+    open var menu: UITableView?
+    var offset: CGPoint?
+    var items:[SPMenuData<T>]?
+    
+    open var selectItem:((T?)->Void)?
+    
+    public convenience init(frame: CGRect, offSet: CGPoint? = nil) {
         self.init(frame: frame)
         self.offset = offSet
         setup()
@@ -15,7 +30,7 @@ open class SPMenu: UIView {
         super.init(frame: frame)
     }
     
-    private func setup() {
+    private func drawBackground() {
         dim.frame = self.frame
         dim.backgroundColor = .blue
         dim.alpha = 0.2
@@ -24,11 +39,26 @@ open class SPMenu: UIView {
         dim.addGestureRecognizer(ges)
         
         self.addSubview(dim)
-        
-        menu = UITableView(frame: CGRect(x: offset?.x ?? 0, y: offset?.y ?? 0, width: 100, height: 100))
+    }
+    
+    public func reloadData(data: [SPMenuData<T>]) {
+        self.items = data
+        self.menu?.reloadData()
+    }
+    
+    private func drawMenu() {
+        menu = UITableView(frame: CGRect(x: offset?.x ?? 0, y: offset?.y ?? 0, width: 200, height: 200))
         if let m = menu {
             self.addSubview(m)
         }
+        
+        menu?.delegate = self
+        menu?.dataSource = self
+    }
+    
+    private func setup() {
+        drawBackground()
+        drawMenu()
     }
     
     @objc private func tapHandler() {
@@ -38,4 +68,22 @@ open class SPMenu: UIView {
     public required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.items?[indexPath.row].title
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.menu?.deselectRow(at: indexPath, animated: true)
+        self.selectItem?(self.items?[indexPath.row].data)
+        self.removeFromSuperview()
+    }
+    
 }
+
